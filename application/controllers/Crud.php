290dfbc90e32521c5df7e44a1607b53f7021ffd5
Jason Kolenosky <JasonK58@gmail.com>
2016-11-10 15:29:53 -0800

    Set up menu items for admin.
    Signed-off-by: Jason Kolenosky <JasonK58@gmail.com>

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -4,15 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Crud extends Application {
 
-	public function index()
-	{
-		$userrole = $this->session->userdata('userrole');
-		if ($userrole != 'admin')
-			$message = 'You are not authorized to access this page. Go away';
-		else
-			$message = 'Get ready to fix stuff.';
-		$this->data['content'] = $message;
-		$this->render();
-	}
+	public function index() {
+            $userrole = $this->session->userdata('userrole');
+            if ($userrole != 'admin') {
+                $message = 'You are not authorized to access this page. Go away';
+                $this->data['content'] = $message;
+            }
+            $message = 'Get ready to fix stuff.';
+            $this->data['pagebody' ] ='mtce';
+            $this->data['items'] = $this->menu->all();
+            $this->render();
+        }
 
 }
