dbae2a866b1ec6d0fff63c283c6160d77f54467c
Jason Kolenosky <JasonK58@gmail.com>
2016-11-10 15:48:17 -0800

    Created view to edit menu items from admin maintenance view.
    Signed-off-by: Jason Kolenosky <JasonK58@gmail.com>

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -15,5 +15,33 @@ class Crud extends Application {
             $this->data['items'] = $this->menu->all();
             $this->render();
         }
+        
+        function edit($id=null) {
+            $this->load->helper('formfields');
+            
+            // try the session first
+            $key = $this->session->userdata('key');
+            $record = $this->session->userdata('record');
+            
+            // if not there, get them from the database
+            if (empty($key)) {
+                $record = $this->menu->get($id);
+                $key = $id;
+                $this->session->set_userdata('key',$id);
+                $this->session->set_userdata('record',$record);
+            }
+            
+            // build the form fields
+            $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+            $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+            $this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
+            $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+            $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+            $this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
+            
+            // show the editing form
+            $this->data['pagebody'] = "mtce-edit";
+            $this->render();
+}
 
 }
