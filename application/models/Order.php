8b7b983a82c9c525b68ec012b127c757fc264d3b
Daniel Lin <danlin604@gmail.com>
2016-11-18 12:21:42 -0800

    Adding order num
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- application/models/Order.php
+++ application/models/Order.php
@@ -37,11 +37,14 @@ class Order extends CI_Model {
         }
     }
 
-    public function receipt() {
+    public function receipt() {        
         $total = 0;
         $result = $this->data['pagetitle'] . '  ' . PHP_EOL;
         $result .= date(DATE_ATOM) . PHP_EOL;
         $result .= PHP_EOL . 'Your Order:'. PHP_EOL . PHP_EOL;
+        if($this->number != 0) {
+            $result .= PHP_EOL . $this->number . PHP_EOL . PHP_EOL;
+        }
         foreach($this->items as $key => $value) {
             $menu = $this->menu->get($key);
             $result .= '- ' . $value . ' ' . $menu->name . PHP_EOL;
